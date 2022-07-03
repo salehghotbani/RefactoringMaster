0652d22237efcf294b3ca64dd2198c3e9a5e6474
salehghotbani <saleh313ghotbani@gmail.com>
2022-07-03 23:41:18 +0430

    Final edition! Amen

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -17,63 +17,71 @@ public class EnrollCtrl {
             Map<Term, Map<Course, Double>> transcript = s.getTranscript(); //lists of courses that student passed
             for (CSE o : courses) {
                 //check for student passed a lesson or not
-                for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-                    for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-                        if (r.getKey().equals(o.getCourse()) && r.getValue() >= 10)
-                            new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
-                    }
-                }
+                studentPassed(transcript, o);
                 //check prerequisite of courses that student requested
-                List<Course> prereqs = o.getCourse().getPrerequisites();
-                nextPre:
-                for (Course pre : prereqs) {
-                    for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-                        for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-                            if (r.getKey().equals(pre) && r.getValue() >= 10)
-                                continue nextPre;
-                        }
-                    }
-                    new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
-                }
-
-                for (CSE o2 : courses) {
-                    if (o == o2)
-                        continue;
-                    //check time of exam, to not same as another lessons exam time
-                    if (o.getExamTime().equals(o2.getExamTime()))
-                        new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", o, o2));
-                    //check if request lesson was taken before
-                    if (o.getCourse().equals(o2.getCourse()))
-                        new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
-                }
+                coursesPrerequisite(transcript, o);
+                //check time of exam, to not same as another lessons exam time or check if request lesson was taken before
+                sameTimeTakenBefore(courses, o);
             }
 
-            // numbers of units that requested
-            int unitsRequested = 0;
-            for (CSE o : courses)
-                unitsRequested += o.getCourse().getUnits();
-
-            // Grade point average
-            double points = 0;
-            int totalUnits = 0;
-            for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-                for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-                    points += r.getValue() * r.getKey().getUnits();
-                    totalUnits += r.getKey().getUnits();
-                }
-            }
-            double gpa = points / totalUnits;
-
-            /*check that when student's gpa is less than 12 he(\she) cannot request unit greater than 14,
-             * and when gpa is less than 16 he(\she) cannot request unit greater than 16,
-             * and anyway it cannot greater than 20!*/
-            if ((gpa < 12 && unitsRequested > 14) || (gpa < 16 && unitsRequested > 16) || (unitsRequested > 20))
-                new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
-            for (CSE o : courses)
-                s.takeCourse(o.getCourse(), o.getSection());
+            GPAChecker(s, courses);
 
         } catch (StudentNotFoundException msg) {
-            JOptionPane.showMessageDialog(null,"Student Not Found Exception occurred!\nMessage: " + msg, "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Student Not Found Exception occurred!\nMessage: " + msg, "Error", JOptionPane.ERROR_MESSAGE);
         }
+    }
+
+    private void sameTimeTakenBefore(List<CSE> courses, CSE o) {
+        for (CSE o2 : courses) {
+            if (o == o2)
+                continue;
+            //check time of exam, to not same as another lessons exam time
+            if (o.getExamTime().equals(o2.getExamTime()))
+                new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", o, o2));
+            //check if request lesson was taken before
+            if (o.getCourse().equals(o2.getCourse()))
+                new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
+        }
+    }
+
+    private void coursesPrerequisite(Map<Term, Map<Course, Double>> transcript, CSE o) {
+        List<Course> prereqs = o.getCourse().getPrerequisites();
+        nextPre:
+        for (Course pre : prereqs) {
+            for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+                for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+                    if (r.getKey().equals(pre) && r.getValue() >= 10)
+                        continue nextPre;
+                }
+            }
+            new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
+        }
+    }
+
+    private void studentPassed(Map<Term, Map<Course, Double>> transcript, CSE o) {
+        for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+            for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+                if (r.getKey().equals(o.getCourse()) && r.getValue() >= 10)
+                    new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
+            }
+        }
+    }
+
+    private void GPAChecker(Student s, List<CSE> courses) {
+        // numbers of units that requested
+        int unitsRequested = 0;
+        for (CSE o : courses)
+            unitsRequested += o.getCourse().getUnits();
+
+        // Grade point average
+        double gpa = s.getGPA();
+
+        /*check that when student's gpa is less than 12 he(\she) cannot request unit greater than 14,
+         * and when gpa is less than 16 he(\she) cannot request unit greater than 16,
+         * and anyway it cannot greater than 20!*/
+        if ((gpa < 12 && unitsRequested > 14) || (gpa < 16 && unitsRequested > 16) || (unitsRequested > 20))
+            new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
+        for (CSE o : courses)
+            s.takeCourse(o.getCourse(), o.getSection());
     }
 }
--- src/domain/Student.java
+++ src/domain/Student.java
@@ -11,6 +11,19 @@ public class Student {
     private final Map<Term, Map<Course, Double>> transcript; // courses that passed with this student
     private final List<CourseSection> currentTerm; // lists of courses in current term
 
+    double getGPA() {
+        Map<Term, Map<Course, Double>> transcript = getTranscript();
+        double points = 0;
+        int totalUnits = 0;
+        for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+            for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+                points += r.getValue() * r.getKey().getUnits();
+                totalUnits += r.getKey().getUnits();
+            }
+        }
+        return points / totalUnits;
+    }
+
     static class CourseSection {
         Course course;
         int section;
